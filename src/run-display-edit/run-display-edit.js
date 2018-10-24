605c0f1d202a543c7530ee6ed920af59c6ff81a8
Patrick Fichtner <fichtner.patrick@dh-karlsruhe.de>
2018-10-24 12:21:46 +0200

    Patrick: km/h-Berechnung aufgrund Strecke und Dauer in Detailansicht ergänzt, Ändern-Event, Abbrechen-Event und Sichern-Event der Detailansicht hinzugefügt.

--- src/run-display-edit/run-display-edit.js
+++ src/run-display-edit/run-display-edit.js
@@ -85,15 +85,74 @@ class RunDisplayEdit {
         alert(run.art);
         alert(run.minutenPerKm);*/
 
-        //let inputdate = run.datum.toString();
+        document.getElementById('Name').value=run.name;
         document.getElementById('Datum').value=run.datum;
         document.getElementById('Distanz').value=run.strecke;
         document.getElementById('Art').value=run.art;
         document.getElementById('Zeit').value=run.dauer;
         document.getElementById('minutenPerKm').value=run.minutenPerKm;
-        document.getElementById('kilometerPerStd').value=run.kilometerPerStd;
+        //alert(run.strecke);
+
+        /*Kommazahl zur Berechnung der Km/h in Wert mit Punkt umwandeln*/
+        let meter = run.strecke.replace( /,/,"." );
+            meter = parseFloat(meter)*1000;
+        let sekunde = run.dauer.replace( /,/,"." );
+            sekunde = parseFloat(run.dauer)*60;
+        let kmPerStd = ((meter/sekunde)/1000)/(1/3600);
+
+        document.getElementById('kilometerPerStd').value=kmPerStd;
         //document.getElementById('Beschreibung').value=
         //document.getElementById('Bewertung').value=
+
+        /*Event für Bearbeiten-Button*/
+        section.querySelector("#bearbeiten").addEventListener("click",() => {
+            document.getElementById('Name').removeAttribute('disabled');
+            document.getElementById('Datum').removeAttribute('disabled');
+            document.getElementById('Distanz').removeAttribute('disabled');
+            document.getElementById('Zeit').removeAttribute('disabled');
+            document.getElementById('Art').removeAttribute('disabled');
+            document.getElementById('minutenPerKm').removeAttribute('disabled');
+            document.getElementById('kilometerPerStd').removeAttribute('disabled');
+            document.getElementById('Beschreibungstext').removeAttribute('disabled');
+        });
+        /*Event für Abbrechen-Button*/
+        section.querySelector("#abbrechen").addEventListener("click",() => {
+            document.getElementById('Name').setAttribute('disabled', 'disabled');
+            document.getElementById('Datum').setAttribute('disabled', 'disabled');
+            document.getElementById('Distanz').setAttribute('disabled', 'disabled');
+            document.getElementById('Zeit').setAttribute('disabled', 'disabled');
+            document.getElementById('Art').setAttribute('disabled', 'disabled');
+            document.getElementById('minutenPerKm').setAttribute('disabled', 'disabled');
+            document.getElementById('kilometerPerStd').setAttribute('disabled', 'disabled');
+            document.getElementById('Beschreibungstext').setAttribute('disabled', 'disabled');
+        });
+        /*Event für Sichern-Button*/
+        section.querySelector("#sichern").addEventListener("click",() => {
+            //alert(run.datum);
+            let changeName = document.querySelector("#Name").value;
+            let changeDatum = document.querySelector("#Datum").value;
+            let changeDistanz = document.querySelector("#Distanz").value;
+            let changeZeit = document.querySelector("#Distanz").value;
+            let changeArt = document.querySelector("#Art").value;
+            let changeMinutenPerKm = document.querySelector("#minutenPerKm").value;
+            //let changeKilometerperStd = document.querySelector("#KilometerPerStd").value;
+            let changeBeschreibung = document.querySelector("#Beschreibungstext").value;
+
+
+            this._runsDB.update({
+                name: changeName,
+                strecke: changeDistanz,
+                dauer: changeZeit,
+                minutenPerKm: changeMinutenPerKm,
+                art: changeArt,
+                datum: changeDatum,
+            });
+            //alert(run.name);
+            //run.datum.update("1");
+            //alert(run.datum);
+            //update(this._runsDB);
+            //alert(this._runsDB);
+        });
 
 
         return {
@@ -101,7 +160,12 @@ class RunDisplayEdit {
             topbar: section.querySelectorAll("header > *"),
             main: section.querySelectorAll("main > *"),
         };
+
     }
+
+
+
+
 
 
 
